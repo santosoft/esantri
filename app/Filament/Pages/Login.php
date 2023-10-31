74e576a8fa15fe70e8957797ed17c99e24563e41
santosoft <alpha.digit@gmail.com>
2023-10-31 17:54:25 +0700

    fix: missing login exception

--- app/Filament/Pages/Login.php
+++ app/Filament/Pages/Login.php
@@ -5,6 +5,7 @@ namespace App\Filament\Pages;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Components\TextInput;
 use Filament\Pages\Auth\Login AS FilamentLogin;
+use Illuminate\Validation\ValidationException;
 
 class Login extends FilamentLogin
 {
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,7 @@ class UserFactory extends Factory
             'username' => strtolower(str_replace(' ', '', $name)),
             'email' => strtolower(str_replace(' ', '', $name)).'@example.com',
             // 'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'), // password
             'remember_token' => Str::random(10),
             'role' => $roles[rand(0,3)]
         ];
